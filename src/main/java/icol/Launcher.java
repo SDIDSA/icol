100c3430493d34459ee374637b398fcdd3798a55
SDIDSA <zinou.teyar@gmail.com>
2023-05-06 10:31:36 +0100

    clean up

--- src/main/java/icol/Launcher.java
+++ src/main/java/icol/Launcher.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 
 public class Launcher {
 	public static void main(String[] args) {
-		System.out.println(AdminCheck.IS_RUNNING_AS_ADMINISTRATOR);
 		 Application.launch(Icol.class, args);
 	}
 }
